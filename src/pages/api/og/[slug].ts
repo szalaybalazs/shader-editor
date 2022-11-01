a520ed7af3518d89d547cb66bb947ccad60ae37f
Szalay Balázs <szalay.balazs.ms@gmail.com>
2022-11-01 18:01:37 +0100

    remove puppeteer

--- src/pages/api/og/[slug].ts
+++ src/pages/api/og/[slug].ts
@@ -7,40 +7,41 @@ const banner = async (req: NextApiRequest, res: NextApiResponse) => {
     const { slug } = req.query;
 
     console.log(await chrome.executablePath, chrome);
-    // const browser = await puppeteer.launch({
-    //   args: chrome.args,
-    //   executablePath: await chrome.executablePath,
-    //   headless: chrome.headless,
-    //   defaultViewport: { width: 1024, height: 1024 },
-    // });
+    // // const browser = await puppeteer.launch({
+    // //   args: chrome.args,
+    // //   executablePath: await chrome.executablePath,
+    // //   headless: chrome.headless,
+    // //   defaultViewport: { width: 1024, height: 1024 },
+    // // });
 
-    const browser = await puppeteer.launch(
-      process.env.NODE_ENV === 'production'
-        ? {
-            args: chrome.args,
-            executablePath: await chrome.executablePath,
-            headless: chrome.headless,
-          }
-        : {},
-    );
+    // const browser = await puppeteer.launch(
+    //   process.env.NODE_ENV === 'production'
+    //     ? {
+    //         args: chrome.args,
+    //         executablePath: await chrome.executablePath,
+    //         headless: chrome.headless,
+    //       }
+    //     : {},
+    // );
 
-    const page = await browser.newPage();
+    // const page = await browser.newPage();
 
-    const url = `https://shader-editor.vercel.app/${slug}/full?clear=true&banner=true`;
+    // const url = `https://shader-editor.vercel.app/${slug}/full?clear=true&banner=true`;
 
-    console.log(url);
+    // console.log(url);
 
-    await page.goto(url);
+    // await page.goto(url);
 
-    const screenshot = await page.screenshot();
+    // const screenshot = await page.screenshot();
 
-    await page.close();
-    console.log('pupeteer');
+    // await page.close();
+    // console.log('pupeteer');
 
-    await browser.close();
+    // await browser.close();
 
-    res.setHeader('Content-Type', 'image/png');
-    res.send(screenshot);
+    // res.setHeader('Content-Type', 'image/png');
+    // res.send(screenshot);
+    res.status(200).json({ error: true });
   } catch (error) {
     res.status(502).json({ error: error.message });
   }
