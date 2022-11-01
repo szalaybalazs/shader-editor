1c37ab4cd81ac7300db3be8b7c0ec38d18552445
Szalay Balázs <szalay.balazs.ms@gmail.com>
2022-11-01 17:30:43 +0100

    vercel chromium

--- src/pages/api/og/[slug].ts
+++ src/pages/api/og/[slug].ts
@@ -1,16 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import puppeteer from 'puppeteer';
-import chromium from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
+import chrome from 'chrome-aws-lambda';
 
 const shaders = async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug } = req.query;
 
   const browser = await puppeteer.launch({
     defaultViewport: { width: 1024, height: 1024 },
-    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-    executablePath: await chromium.executablePath,
-    headless: true,
-    ignoreHTTPSErrors: true,
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
   });
 
   const page = await browser.newPage();
